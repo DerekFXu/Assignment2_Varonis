b007d72a0b52f8b63d02e20dfe6db571d276ba03
Derek Xu <derekfxu@gmail.com>
2022-07-12 10:01:59 -0400

    Update __init__.py

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -1,10 +1,10 @@
 import logging
+import requests
 
 import azure.functions as func
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from datetime import datetime
-from requests.exceptions import RequestException
 
 credential = DefaultAzureCredential(managed_identity_client_id = "efdd8d32-9ad9-427a-ba4d-12ef2517c8f5")
 client = SecretClient(
@@ -31,7 +31,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             vault_name = vault_name_first.replace(".vault.azure.net/", "")
             secret_time = secret.properties.created_on.strftime("%m/%d/%Y, %H:%M:%S")
             return func.HttpResponse(f"Name of the Key Vault: {vault_name}\nName of the Key Vault Secret: {secret.name}\nCreation Date Of Secret: {secret_time}\nValue Of Secret: {secret.value}\n{vault_name_first}\n{secret.properties.vault_url}")
-        except RequestException as e:
+        except requests.HTTPError as e:
             return func.HttpResponse(f"Sorry this is not a valid secret name, error: {e}")
     else:
         return func.HttpResponse(
