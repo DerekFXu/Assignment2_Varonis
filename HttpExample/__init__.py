167ffba2801182d20a24c5c7769c7c20402696e4
Derek Xu <derekfxu@gmail.com>
2022-07-12 09:26:16 -0400

    Adding try-catch error

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -4,6 +4,7 @@ import azure.functions as func
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from datetime import datetime
+from urllib.error import HTTPError
 
 credential = DefaultAzureCredential(managed_identity_client_id = "efdd8d32-9ad9-427a-ba4d-12ef2517c8f5")
 client = SecretClient(
@@ -24,11 +25,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        secret = client.get_secret(name)
-        vault_name_first = secret.properties.vault_url.replace("https://", "")
-        vault_name = vault_name_first.replace(".vault.azure.net/", "")
-        secret_time = secret.properties.created_on.strftime("%m/%d/%Y, %H:%M:%S")
-        return func.HttpResponse(f"Name of the Key Vault: {vault_name}\nName of the Key Vault Secret: {secret.name}\nCreation Date Of Secret: {secret_time}\nValue Of Secret: {secret.value}")
+        try:
+            secret = client.get_secret(name)
+            vault_name_first = secret.properties.vault_url.replace("https://", "")
+            vault_name = vault_name_first.replace(".vault.azure.net/", "")
+            secret_time = secret.properties.created_on.strftime("%m/%d/%Y, %H:%M:%S")
+            return func.HttpResponse(f"Name of the Key Vault: {vault_name}\nName of the Key Vault Secret: {secret.name}\nCreation Date Of Secret: {secret_time}\nValue Of Secret: {secret.value}\n{vault_name_first}")
+        except HTTPError:
+            return func.HttpResponse("Sorry this is not a valid secret name")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
