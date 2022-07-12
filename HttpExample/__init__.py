42852840c82b871174b0ecf3b7a739137a5bc002
Derek Xu <derekfxu@gmail.com>
2022-07-12 11:10:28 -0400

    Update __init__.py

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -5,6 +5,8 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from datetime import datetime
 
+
+#Credientials to access key vault
 credential = DefaultAzureCredential(managed_identity_client_id = "efdd8d32-9ad9-427a-ba4d-12ef2517c8f5")
 client = SecretClient(
     vault_url="https://varonisassignmentkv1-dfx.vault.azure.net/",
@@ -24,17 +26,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
+        #Tries to get secret, returns generic error if secret doesn't exist
         try:
             secret = client.get_secret(name)
         except Exception as e:
-            return func.HttpResponse(f"Sorry this is not a valid secret name\nException:{e}")
-            
+            return func.HttpResponse("Generic Error")
+    
         vault_name_first = secret.properties.vault_url.replace("https://", "")
+        #This line and above strip the url to give just the key vault name. But this second replace function doesn't work properly even though it works fine in a pyhton interpreter. I don't know why
         vault_name = vault_name_first.replace(".vault.azure.net/", "")
+        #Converts created_on to string
         secret_time = secret.properties.created_on.strftime("%m/%d/%Y, %H:%M:%S")
         return func.HttpResponse(f"Name of the Key Vault: {vault_name}\nName of the Key Vault Secret: {secret.name}\nCreation Date Of Secret: {secret_time}\nValue Of Secret: {secret.value}\n{vault_name_first}")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
