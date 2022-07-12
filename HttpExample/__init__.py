703a4fef1ee8c203b57d31ee0343cae16e1e7127
Derek Xu <derekfxu@gmail.com>
2022-07-12 09:57:20 -0400

    Using requests module instead

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from datetime import datetime
-from urllib.error import HTTPError
+from requests.exceptions import RequestException
 
 credential = DefaultAzureCredential(managed_identity_client_id = "efdd8d32-9ad9-427a-ba4d-12ef2517c8f5")
 client = SecretClient(
@@ -31,8 +31,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             vault_name = vault_name_first.replace(".vault.azure.net/", "")
             secret_time = secret.properties.created_on.strftime("%m/%d/%Y, %H:%M:%S")
             return func.HttpResponse(f"Name of the Key Vault: {vault_name}\nName of the Key Vault Secret: {secret.name}\nCreation Date Of Secret: {secret_time}\nValue Of Secret: {secret.value}\n{vault_name_first}\n{secret.properties.vault_url}")
-        except HTTPError:
-            return func.HttpResponse("Sorry this is not a valid secret name")
+        except RequestException as e:
+            return func.HttpResponse(f"Sorry this is not a valid secret name, error: {e}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
